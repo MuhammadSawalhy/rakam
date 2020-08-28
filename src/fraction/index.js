abeefb967a1d18261f44b554faba0a58ba11a87c
Mohammed Samir <42011920+ms2052001@users.noreply.github.com>
2020-08-29 01:32:07 +0200

    Create directory fraction with empty exported object

--- src/fraction/index.js
+++ src/fraction/index.js
@@ -1,20 +1,25 @@
-function fraction(num) {
-  if (num.toString().indexOf(".") > -1) {
-    let num1 = parseInt(num.toString().replace(".", "")),
-      num2 = Math.pow(10, num.toString().split(".")[1].length);
-    let gcd_ = this.gcd(num1, num2);
-    return { numerator: num1 / gcd_, denominator: num2 / gcd_ };
-  } else return { numerator: num, denominator: 1 };
-}
 
-function quotientRemainder(num) {
-  if (num.toString().indexOf(".") > -1) {
-    let num1 = parseInt(num.toString().split(".")[1]),
-      num2 = Math.pow(10, num1.toString().length);
+// TODO: use fraction.js npm package, and fork making fraction-latex.js
+// export function fraction(num) {
+//   if (num.toString().indexOf(".") > -1) {
+//     let num1 = parseInt(num.toString().replace(".", "")),
+//       num2 = Math.pow(10, num.toString().split(".")[1].length);
+//     let gcd_ = this.gcd(num1, num2);
+//     return { numerator: num1 / gcd_, denominator: num2 / gcd_ };
+//   } else return { numerator: num, denominator: 1 };
+// }
 
-    num = parseInt(num.toString().split(".")[0]);
-    let gcd_ = this.gcd2(num1, num2);
+// export function quotientRemainder(num) {
+//   if (num.toString().indexOf(".") > -1) {
+//     let num1 = parseInt(num.toString().split(".")[1]),
+//       num2 = Math.pow(10, num1.toString().length);
 
-    return { quotient: num, numerator: num1 / gcd_, denominator: num2 / gcd_ };
-  } else return { quotient: num, numerator: 0, denominator: 1 };
-}
+//     num = parseInt(num.toString().split(".")[0]);
+//     let gcd_ = this.gcd2(num1, num2);
+
+//     return { quotient: num, numerator: num1 / gcd_, denominator: num2 / gcd_ };
+//   } else return { quotient: num, numerator: 0, denominator: 1 };
+// }
+
+
+export default {};
